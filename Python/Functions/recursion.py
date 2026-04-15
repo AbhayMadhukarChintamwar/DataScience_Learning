82b5a8383ac79b2047d3b313f7639cc57fc3999a
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-15 10:25:12 +0530

    Add factorial and recursion function implementations

---
+++ Python/Functions/recursion.py
@@ -0,0 +1,17 @@
+
+import sys
+from time import sleep
+
+sys.setrecursionlimit(11)
+print(sys.getrecursionlimit())
+
+count =1
+
+def greet():
+    global  count
+    print("Hello!", count)
+    count += 1
+    sleep(0.02)
+    greet()
+
+greet()
