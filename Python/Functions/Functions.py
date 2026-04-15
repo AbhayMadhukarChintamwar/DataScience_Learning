5e70e3c7227c4f9ac1e2550a6d07ccebe12d70d5
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-15 08:28:38 +0530

    Refactor person function and clarify usage of keyword arguments

--- Python/Functions/Functions.py
+++ Python/Functions/Functions.py
@@ -1,15 +1,21 @@
 
+def person(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+person(age = 24, name = "Abhay")  # keyword arguments, the order of arguments does not matter when calling the function with keyword arguments
+
 
 
 # def add(num1, num2):     # define a function named add that takes two parameters num1 and num2
 #     return num1 + num2
 
 
-def add(num1, *num2):   # variable length arguments, num2 can take any number of arguments
-    total = num1  # initialize total with the value of num1
-    for n in num2:  # iterate over each number in num2
-        total += n  # add each number to total
-    return total  # return the final total
+# def add(num1, *num2):   # variable length arguments, num2 can take any number of arguments
+#     total = num1  # initialize total with the value of num1
+#     for n in num2:  # iterate over each number in num2
+#         total += n  # add each number to total
+#     return total  # return the final total
 
-result = add(10, 20, 30, 40)  # call the add function with multiple arguments
-print(result)  # print the result 
+# result = add(10, 20, 30, 40)  # call the add function with multiple arguments
+# print(result)  # print the result
