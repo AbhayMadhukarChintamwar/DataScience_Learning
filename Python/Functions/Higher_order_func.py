5f410d0f70c44dae82eba361c4e7b0a100a487ef
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:09:19 +0530

    Refactor operate function to handle lists and print results for square and cube operations

--- Python/Functions/Higher_order_func.py
+++ Python/Functions/Higher_order_func.py
@@ -6,12 +6,21 @@ def cube(num):
 
 
 
-def operate(num, operation): # this is a higher order function because it takes another function as an argument
-    return operation(num)    # this will call the operation function with num as an argument and return the result
+# def operate(num, operation): # this is a higher order function because it takes another function as an argument
+#     return operation(num)    # this will call the operation function with num as an argument and return the result
 
 
-value = 5
-result = operate(value,square)
-result1 = operate(value,cube)
-print(result)  # This will print 25
-print(result1)    # This will print 125
+# value = 5
+# result = operate(value,square)
+# print(result)  # this will print the square of 5 which is 25
+
+
+
+def operate(nums, operation):
+    for i in nums:
+        result = operation(i) # this will call the operation function with i as an argument and return the result
+        print(result)
+
+nums = [5,6,7,8,9]
+operate(nums, square)  # this will print the square of each number in the nums list
+operate(nums, cube)  # this will print the cube of each number in the nums list
