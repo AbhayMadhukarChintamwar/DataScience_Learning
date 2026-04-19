8ae9f9d7acb06399595109441a822c73dce741e1
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:17:13 +0530

    Fix formatting and add print statement for addition result in lambda demo

--- Python/Functions/lambd_demo.py
+++ Python/Functions/lambd_demo.py
@@ -3,7 +3,10 @@
 #     return num * num
 
 func = lambda num: num * num # lambda is an anonymous function, it can take any number of arguments but can only have one expression. The expression is evaluated and returned.
-
+add = lambda x, y : x + y # lambda can also be used to create simple functions that can be used in higher-order functions like map, filter, and reduce.
 
 result = func(5)
+print(result)
+
+result = add(3, 4)
 print(result)
