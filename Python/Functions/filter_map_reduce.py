8d96bac2dda2dd610cea81156e30fe2ed8fc1e2d
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:36:32 +0530

    Refactor even number filtering to use filter function with is_even predicate

--- Python/Functions/filter_map_reduce.py
+++ Python/Functions/filter_map_reduce.py
@@ -1,9 +1,15 @@
 nums = [1,2,3,4,5,6,7,8,9]
-evens = []
+# evens = []
 
-for i in nums:
-    if i % 2 == 0:
-        evens.append(i) # this is a simple way to filter out even numbers from a list. We can also use the filter function to achieve the same result in a more concise way.
+# for i in nums:
+#     if i % 2 == 0:
+#         evens.append(i) # this is a simple way to filter out even numbers from a list. We can also use the filter function to achieve the same result in a more concise way.
 
 
-print(evens)
+def is_even(n):
+    return  n % 2 == 0
+
+evens = list(filter(is_even, nums))  # filter takes a function and an iterable and returns an iterator that produces those items of the iterable for which the function returns true. In this case, we are using the is_even function to filter out the even numbers from the nums list.
+
+
+print(evens) 
