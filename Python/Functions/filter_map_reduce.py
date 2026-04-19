61f75e5bcd37e646654c2e27bcb3cc7aed1c665f
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:40:17 +0530

    Refactor even number filtering to use a lambda function instead of a separate is_even function

--- Python/Functions/filter_map_reduce.py
+++ Python/Functions/filter_map_reduce.py
@@ -6,10 +6,12 @@ nums = [1,2,3,4,5,6,7,8,9]
 #         evens.append(i) # this is a simple way to filter out even numbers from a list. We can also use the filter function to achieve the same result in a more concise way.
 
 
-def is_even(n):
-    return  n % 2 == 0
+# def is_even(n):
+#     return  n % 2 == 0
+
+is_even = lambda n: n % 2 == 0
 
 evens = list(filter(is_even, nums))  # filter takes a function and an iterable and returns an iterator that produces those items of the iterable for which the function returns true. In this case, we are using the is_even function to filter out the even numbers from the nums list.
 
 
-print(evens) 
+print(evens)
