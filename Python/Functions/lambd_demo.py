82656753f88d2884c791a3e7c0a3227fc0846ada
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:14:55 +0530

    Add lambda function example for squaring a number

---
+++ Python/Functions/lambd_demo.py
@@ -0,0 +1,9 @@
+
+# def func(num):
+#     return num * num
+
+func = lambda num: num * num # lambda is an anonymous function, it can take any number of arguments but can only have one expression. The expression is evaluated and returned.
+
+
+result = func(5)
+print(result)
