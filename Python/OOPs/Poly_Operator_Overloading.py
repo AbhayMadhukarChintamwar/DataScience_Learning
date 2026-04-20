25a9b6f29a87ac40c02a49cd5f0baba8df249a7e
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-20 10:37:30 +0530

    Add operator overloading in polymorphism

--- Python/OOPs/Poly_Operator_Overloading.py
+++ Python/OOPs/Poly_Operator_Overloading.py
@@ -10,12 +10,20 @@ class Account:
     def __add__(self, other): # operator overloading for addition of two objects
         return Account('combined', self.balance + other.balance)
 
+    def __gt__(self, other): # operator overloading for greater than comparison of two objects
+        return self.balance > other.balance
+
 
 user1 = Account('User1', 1000)
-user2 = Account('User2', 1000)
+user2 = Account('User2', 2000)
 
 combined = user1 + user2
 
 print(user1)
 print(user2)
 print(combined)
+
+if user1 > user2:
+    print(f'{user1.name} has more balance than {user2.name}')
+else:
+    print(f'{user2.name} has more balance than {user1.name}')
