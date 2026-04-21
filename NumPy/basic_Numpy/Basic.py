612c29f9cd71926abf87ba55a3b743bd457ff35a
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-21 10:58:35 +0530

    Add examples for linspace and logspace functions in NumPy; improve output clarity

--- NumPy/basic_Numpy/Basic.py
+++ NumPy/basic_Numpy/Basic.py
@@ -26,3 +26,20 @@ print(range) # Output: [1 2 3 4 5 6 7 8 9]
 
 range = np.arange(1,10,2) # This creates a 1D array with values starting from 1 up to (but not including) 10, with a step of 2.
 print(range) # Output: [1 3 5 7 9]
+
+
+
+
+# np.license(start, end, num)
+# This is a function that generates linearly spaced values between a specified start and end point. The 'num' parameter specifies the number of values to generate. The resulting array will contain 'num' evenly spaced values between 'start' and 'end', inclusive.
+
+lin_space = np.linspace(0,1,5) # This creates a 1D array with 5 evenly spaced values between 0 and 1 (inclusive). The linspace function is used to generate linearly spaced values between a specified start and end point. In this case, it generates 5 values starting from 0 and ending at 1, with equal spacing between them.
+print(lin_space) # Output: [0.  0.25 0.5  0.75 1.] This creates a 1D array with 5 evenly spaced values between 0 and 1 (inclusive).
+
+
+
+#  np.logspace(start, end-> powers, number of values)
+# This is a function that generates logarithmically spaced values between a specified start and end point. The 'start' and 'end' parameters specify the powers of 10 for the start and end points, respectively. The 'num' parameter specifies the number of values to generate. The resulting array will contain 'num' logarithmically spaced values between 10^start and 10^end, inclusive.
+
+Log_Space = np.logspace(1,3,3) # This creates a 1D array with 3 logarithmically spaced values between 10^1 (which is 10) and 10^3 (which is 1000). The logspace function is used to generate logarithmically spaced values between a specified start and end point. In this case, it generates 3 values starting from 10^1 (10) and ending at 10^3 (1000), with equal spacing on a logarithmic scale.
+print(Log_Space) # Output: [  10.  100. 1000.] This creates a 1D array with 3 logarithmically spaced values between 10^1 (which is 10) and 10^3 (which is 1000).
