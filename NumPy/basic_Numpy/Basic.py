c1ad386f097ae9aa8a2e64a8d1b906c08a2220f5
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-21 10:47:27 +0530

    Comment out example code for mixed data types and 2D matrix creation in NumPy; add range array examples.

--- NumPy/basic_Numpy/Basic.py
+++ NumPy/basic_Numpy/Basic.py
@@ -1,20 +1,28 @@
 import numpy as np
-mat = [1,2,3,'gre',1.5] # When we create a numpy array with mixed data types, it will upcast to the most general type that can accommodate all the values. In this case, since we have a string 'gre', the entire array will be upcast to a string type.
-matrix = np.array([1,2,3,'gre',1.5]) # The resulting array will contain all elements as strings: ['1', '2', '3', 'gre', '1.5']. This is because numpy arrays require all elements to be of the same type, and the presence of a string forces the entire array to be treated as strings.
+# mat = [1,2,3,'gre',1.5] # When we create a numpy array with mixed data types, it will upcast to the most general type that can accommodate all the values. In this case, since we have a string 'gre', the entire array will be upcast to a string type.
+# matrix = np.array([1,2,3,'gre',1.5]) # The resulting array will contain all elements as strings: ['1', '2', '3', 'gre', '1.5']. This is because numpy arrays require all elements to be of the same type, and the presence of a string forces the entire array to be treated as strings.
 
-ndMatrix = np.array([[1, 2, 3], [4, 5, 6]]) # This creates a 2D array (matrix) with two rows and three columns.
+# ndMatrix = np.array([[1, 2, 3], [4, 5, 6]]) # This creates a 2D array (matrix) with two rows and three columns.
 
 
-print(matrix.ndim)  # dimension of the array
-print(ndMatrix.ndim)  # dimension of the 2D array
+# print(matrix.ndim)  # dimension of the array
+# print(ndMatrix.ndim)  # dimension of the 2D array
 
-print(matrix.shape) # shape of the array
-print(ndMatrix.shape) # shape of the 2D array
+# print(matrix.shape) # shape of the array
+# print(ndMatrix.shape) # shape of the 2D array
 
-print(type(mat)) # Output: <class 'list'>
-print(type(matrix)) # Output: <class 'numpy.ndarray'>
+# print(type(mat)) # Output: <class 'list'>
+# print(type(matrix)) # Output: <class 'numpy.ndarray'>
 
-print(mat) # Output: <class 'list'> and [1, 2, 3, 'gre', 1.5]
-print(matrix) # Output: ['1' '2' '3' 'gre' '1.5']
-print(ndMatrix) # Output: [[1 2 3]
-                    #      [4 5 6]]
+# print(mat) # Output: <class 'list'> and [1, 2, 3, 'gre', 1.5]
+# print(matrix) # Output: ['1' '2' '3' 'gre' '1.5']
+# print(ndMatrix) # Output: [[1 2 3]
+#                     #      [4 5 6]]
+
+
+
+range = np.arange(1,10) # This creates a 1D array with values from 1 to 9 (10 is exclusive).
+print(range) # Output: [1 2 3 4 5 6 7 8 9]
+
+range = np.arange(1,10,2) # This creates a 1D array with values starting from 1 up to (but not including) 10, with a step of 2.
+print(range) # Output: [1 3 5 7 9]
