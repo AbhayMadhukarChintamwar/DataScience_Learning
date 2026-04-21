3ed65b64511aedb0dccbf740c12b164a9271efd2
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-21 12:53:12 +0530

    Add examples for square roots and exponential functions in NumPy; demonstrate np.sqrt and np.exp

---
+++ NumPy/Moderate/universal_Func.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([1,4,9,16])
+#  Square Roots
+sqrts  = np.sqrt(arr)
+print(sqrts)
+
+sqrts  = arr.astype(np.int64)
+print(sqrts)
+
+# Exponential  e^x = np.exp(arr)
+expo = np.exp([1,2])
+print(expo)
