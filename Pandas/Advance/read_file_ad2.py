9b257dfe9bfb61534250a8846635bda1f1cb4d74
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-23 13:17:00 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Pandas/Advance/read_file_ad2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('fmcg_sales_marketing_profitability_2023_2025.csv')
+
+print(df.head())
+
+data = pd.DataFrame(df)
+
+# Set column as index
+# df.set_index('Net_Revenue_USD', inplace=True)
+
+# Line Graph
+# df.plot(kind='line', title='Sales Over Days')
+
+# Labels
+# plt.xlabel('Marketing_Spend_USD')
+# plt.ylabel('Profit_USD')
+df.plot(kind='bar')
+# Show graph
+plt.show()
